1b2ca74fff55d0b5971f6e6d7cae1abc905814ba
shivam-gogawale <gogavaleshivam@gmail.com>
2025-06-14 14:57:23 +0530

    rout

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/Golden-closet-studio" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
